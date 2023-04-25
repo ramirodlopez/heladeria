9388d34c8316aac5a8e44e9bf91f8428652aac31
Rama <Rama@192.168.1.54>
2023-04-24 23:45:46 -0300

    UpdateHeladeria2018

---
+++ interfaz/src/interfaz/Interfaz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+import Client.NewJFrame;
+/**
+ *
+ * @author Ramiro
+ */
+public class Interfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      NewJFrame ventana = new NewJFrame();
+        ventana.setVisible(true);
+    }
+    
+}
